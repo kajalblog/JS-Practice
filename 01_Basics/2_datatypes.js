43edad7c2568ab6048a9a403ae70f693943f3cd9
kajal parchande <kajal.parchande@greentiger.in>
2024-10-25 20:39:42 +0530

    datatypes

---
+++ 01_Basics/2_datatypes.js
@@ -0,0 +1,17 @@
+let mobile=9876567876;
+let name='Sarita';
+let isLoggedIn=false;
+let state=null;
+
+
+// number
+// BigInt
+// string
+// boolean
+// null
+// undefined
+// symbol
+// object
+
+console.log( typeof typeof isLoggedIn );
+
