8d7b6254d88ba6b0c00969f8e9a4fb6624dd577d
kajal parchande <kajal.parchande@greentiger.in>
2024-10-25 20:07:02 +0530

    Js basic variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 874567;
+let accountEmail = "abc@gmail.com";
+var accountName = "Rajat";
+accountCity = "Urali";
+let accountState;
+
+// accountId=45678;   //Not allowed
+// console.log(accountState);
+// Not prefer to use var
+//  because of block scope & functional scope issue
+
+console.table([
+  accountId,
+  accountEmail,
+  accountName,
+  accountCity,
+  accountState,
+]);
