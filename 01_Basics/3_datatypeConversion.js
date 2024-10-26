440503d18e237db639eafc31ae8b64ba7ba66181
kajal parchande <kajal.parchande@greentiger.in>
2024-10-26 11:01:41 +0530

    conversion of datatypes

---
+++ 01_Basics/3_datatypeConversion.js
@@ -0,0 +1,31 @@
+let age = true;
+let converted = Number(age);
+
+console.log(converted);
+//  console.log(typeof converted);
+
+// "66" =>66
+// ""  =>0
+// null =>0
+// true  =>1
+// false =>0
+
+//  let Id=null;
+//  let convertedString=String(Id);
+
+//  console.log( convertedString);
+//  console.log(typeof Id);
+
+let IsBoolean = "";
+let IsBooleanValue = "Kajal";
+
+//  console.log(!IsBoolean);
+
+let BooleanLognnedIn = Boolean(IsBoolean);
+
+console.log(!!BooleanLognnedIn);
+console.log(!!IsBooleanValue);
+
+//  1 =>true  0=>false
+//  "" =>false
+// "Kajal" =>true
