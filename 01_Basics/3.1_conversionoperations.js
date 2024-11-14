8d04ee7eaa6abf6360e338dc87e251775c4187fe
kajal parchande <kajal.parchande@greentiger.in>
2024-11-14 15:54:32 +0530

    conversion-operators

---
+++ 01_Basics/3.1_conversionoperations.js
@@ -0,0 +1,23 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1 = "hello";
+let str2 = "kajal";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(2 +3);
+// console.log(4 + "1");
+// console.log("2" +5);
+// console.log("2"+3+4);
+// console.log(2+3+"4");
+console.log(2 + "3" + 4);
+
+// if 1st expression is string then others contains as a string for addition only(+)
+
+console.log(4 - 2);
+console.log("4" - 2);
+console.log(4 - "2");
+console.log(4 - "2" - 3);
